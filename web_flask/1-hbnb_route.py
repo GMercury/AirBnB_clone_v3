50743306b742b4fdb20ba3dde4fd021eb609aa6a
GMercury <99487197+GMercury@users.noreply.github.com>
2022-10-24 07:57:58 +0300

    first commit

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,5 +12,11 @@ def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -12,5 +12,17 @@ def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
